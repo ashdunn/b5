2ca899bba0b76f6250a91c9b4877029ce02fd2c5
ashdunn <31973458+ashdunn@users.noreply.github.com>
2020-04-25 19:55:44 -0400

    for real this time

---
+++ Assets/turnLight.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class turnLight : MonoBehaviour {
+
+	// Use this for initialization
+
+	public Light lamp;
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			lamp.enabled = true;
+		}
+	
+	}
+
+	void OnTriggerEnter (Collider other)
+	{
+			lamp.enabled = false;
+	}
+}
