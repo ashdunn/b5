b68196f2477ab3992043f52892d7b7d83b178f24
ashdunn <31973458+ashdunn@users.noreply.github.com>
2020-04-26 22:25:41 -0400

    trigger adjustments

--- Assets/PlayerinRange.cs
+++ Assets/PlayerinRange.cs
@@ -5,49 +5,52 @@ using UnityEngine;
 public class PlayerinRange : MonoBehaviour
 {
     public bool playerinRange = false;
+
+    public bool switchinRange = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-
     }
 
     void OnTriggerEnter(Collider other)
     {
         try
         {
-            playerinRange = (other.transform.parent.name == "Player");     
+            playerinRange = (other.transform.parent.name == "Player");
             // Debug.Log(other.transform.parent.name);
-            if(this.transform.parent.name == "Lamp")
+            if (this.transform.name == "Switch")
             {
+                switchinRange = true;
                 Debug.Log(this.transform.parent.name + ": " + playerinRange);
             }
-            
         }
         catch
-        {}
-
+        {
+        }
     }
 
     void OnTriggerExit(Collider other)
     {
         try
         {
-            // Debug.Log(other.transform.parent.name);
-            playerinRange = !(other.transform.parent.name == "Player");
-            // Debug.Log(this.transform.parent.name + ": " + playerinRange);
-            // QAtrigger = false;
-            if(this.transform.parent.name == "Lamp")
+            if (this.transform.name == "Switch")
             {
-                Debug.Log(this.transform.parent.name + ": " + playerinRange);
+                switchinRange = false;
+                Debug.Log(this.transform.parent.name + ": " + switchinRange);
+            }
+            else if (this.transform.parent.name == "Player")
+            {
+                playerinRange = false;
             }
         }
         catch
-        {}
+        {
+        }
     }
 }
