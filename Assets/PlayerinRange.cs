d54b954b6f14cc18fcce0e0542596c7b43277296
ashdunn <31973458+ashdunn@users.noreply.github.com>
2020-04-26 22:10:07 -0400

    change trigger to switch and set layer to ignore raycast

--- Assets/PlayerinRange.cs
+++ Assets/PlayerinRange.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class PlayerinRange : MonoBehaviour
 {
     public bool playerinRange = false;
-    public bool QAtrigger = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,12 +14,7 @@ public class PlayerinRange : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // if (Input.GetKeyDown(KeyCode.P) & playerinRange)
-        // {
-        //     QAtrigger = true;
-        // }
-        // Debug.Log(this.transform.parent.name + ": " + QAtrigger);
-        
+
     }
 
     void OnTriggerEnter(Collider other)
